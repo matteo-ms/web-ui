030f2a7153764a5ed758c356acdddbdde336a549
mms <matteosimonemirabelli@gmail.com>
2025-08-15 18:18:54 +0200

    new llm models

--- src/utils/config.py
+++ src/utils/config.py
@@ -13,8 +13,8 @@ PROVIDER_DISPLAY_NAMES = {
 
 # Predefined model names for common providers
 model_names = {
-    "anthropic": ["claude-3-5-sonnet-20241022", "claude-3-5-sonnet-20240620", "claude-3-opus-20240229"],
-    "openai": ["gpt-4o", "gpt-4", "gpt-3.5-turbo", "o3-mini"],
+    "anthropic": ["claude-opus-4-1-20250805", "claude-opus-4-20250514", "claude-sonnet-4-20250514","claude-3-7-sonnet-latest"],
+    "openai": ["gpt-4o", "gpt-4.1", "gpt-4.1-mini", "o3-mini","o1"],
     "deepseek": ["deepseek-chat", "deepseek-reasoner"],
     "google": ["gemini-2.0-flash", "gemini-2.0-flash-thinking-exp", "gemini-1.5-flash-latest",
                "gemini-1.5-flash-8b-latest", "gemini-2.0-flash-thinking-exp-01-21", "gemini-2.0-pro-exp-02-05",
